71fafb7888275da5df08c82b9f9eed720dc6cc05
Kate Beard <asouthernbellein@gmail.com>
2018-08-27 19:59:56 +0100

    Move api call to server side

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,19 @@
+const env = require('env2')('./config.env');
+const APPID = process.env.APPID
+const axios = require('axios');
+
 exports.get = (req, res) => {
   res.render('home', { activePage: { home: true } });
 };
+
+exports.post = (req, res) => {
+  const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + req.body.city + '&appid=' + APPID;
+  axios.get(url)
+    .then(response => {
+      console.log('Today there is ' + response.data.weather[0].description + ' in ' + response.data.name + ', and the temperature is currently ' + (Math.floor(response.data.main.temp - 273.15)) + 'C');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  res.render('home', { activePage: { home: true } });
+}
