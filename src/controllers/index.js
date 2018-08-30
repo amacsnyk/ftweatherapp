729520d82a8c3a03816a4be093ed83682148ed22
Kate Beard <asouthernbellein@gmail.com>
2018-08-30 18:02:19 +0100

    Remove unused router path

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,10 +6,6 @@ const error = require('./error');
 
 
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(function (req, res, next) {
-  //function here
-  next()
-})
 router.get('/', home.get);
 router.post('/', home.post);
 router.use(error.client);
